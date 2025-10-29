b5b5e426c7bb70b9cb3c9fb5e8bf68f481bbc5a2
facutorr2005 <facutorresisei@gmail.com>
2025-10-29 19:54:29 -0300

    añadido historial

---
+++ views/historial.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Historial</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="../Public/css/historial.css">
+</head>
+<body>
+    <div class="encabezado">
+        
+        <div class="titulo">
+            Q-Pay
+        </div>
+
+        <button onclick="location.href='panel.php'">
+            Volver al inicio
+        </button>
+
+    </div>
+
+    <div class="historial">
+        <div class="subtitulo">
+            Historial de Compras
+        </div>
+        
+    </div>
+</body>
+</html>
